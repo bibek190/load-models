8c6397ffb4dc5bdefa6ac568755259e969176f16
bibek190 <bibek9818917036@gmail.com>
2025-03-31 00:19:21 +1100

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,43 @@
+import { OrbitControls } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+
+import Model from "./Model";
+import { Suspense } from "react";
+import PlaceHolder from "./PlaceHolder";
+
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+      {/* 
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh> */}
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      <Suspense fallback={<PlaceHolder position-y={1.5} scale={[2, 3, 2]} />}>
+        <Model />
+      </Suspense>
+    </>
+  );
+}
---
+++ src/Model.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { useLoader } from "@react-three/fiber";
+
+const Model = () => {
+  const model = useLoader(
+    GLTFLoader,
+    "./FlightHelmet/glTF/FlightHelmet.gltf",
+    (loader) => {
+      const dracoLoader = new DRACOLoader();
+      dracoLoader.setDecoderPath("./draco/");
+      loader.setDRACOLoader(dracoLoader);
+    }
+  );
+  return (
+    <>
+      <primitive object={model.scene} scale={5} position-y={-1} />
+    </>
+  );
+};
+
+export default Model;
