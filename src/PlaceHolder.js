7e5c473be4fcad2f5609d250e61e7fb918bc4115
bibek190 <bibek9818917036@gmail.com>
2025-04-01 23:59:12 +1100

    added positions

--- src/PlaceHolder.js
+++ src/PlaceHolder.js
@@ -3,9 +3,9 @@ import React from "react";
 const PlaceHolder = (props) => {
   return (
     <>
-      <mesh {...props} position-y={0.5} scale={[2, 3, 2]}>
-        <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
-        <meshBasicMaterial wireframe color="red" />
+      <mesh {...props}>
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshBasicMaterial color="red" />
       </mesh>
     </>
   );
